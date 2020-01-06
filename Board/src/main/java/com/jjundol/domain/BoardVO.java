4f7a9f581cb9a4ad24528cda1232d018d03efb8d
namgo <namgo@JUN-0109>
2020-01-06 20:47:06 +0900

    2020.01.06

---
+++ Board/src/main/java/com/jjundol/domain/BoardVO.java
@@ -0,0 +1,15 @@
+package com.jjundol.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date udpateDate;
+}
---
+++ Board/src/main/java/com/jjundol/mapper/BoardMapper.java
@@ -0,0 +1,14 @@
+package com.jjundol.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.jjundol.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+	
+}
