48517cd82f5ac750404e425050b2e16f67b66191
namgo <namgo@JUN-0109>
2020-01-06 22:28:40 +0900

    2020.01.06 - 게시글 CRUD mapper작성, 테스트

--- Board/src/main/java/com/jjundol/mapper/BoardMapper.java
+++ Board/src/main/java/com/jjundol/mapper/BoardMapper.java
@@ -2,13 +2,26 @@ package com.jjundol.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.jjundol.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+	//@Select("select * from tbl_board where bno > 0")
+	// 게시물 여러개 가져오기
 	public List<BoardVO> getList();
 	
+	// 게시글 작성
+	public void insert(BoardVO board);
+	
+	// 게시글 작성 key
+	public void insertSelectKey(BoardVO board);
+	
+	// 특정 게시글 읽기
+	public BoardVO read(int bno);
+	
+	// 특정 게시글 삭제
+	public int delete(int bno);
+	
+	// 특정 게시글 수정(업데이트)
+	public int update(BoardVO board);
 }
--- Board/src/test/java/com/jjundol/mapper/BoardMapperTests.java
+++ Board/src/test/java/com/jjundol/mapper/BoardMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.jjundol.domain.BoardVO;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -17,9 +19,65 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = { @Autowired })
 	private BoardMapper mapper;
 	
+	
 	@Test
 	public void getList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
 	
+	/*
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글!!!");
+		board.setContent("새로 작성하는 내용!!");
+		board.setWriter("새작성자2");
+		
+		mapper.insert(board);
+		
+		log.info("testInsert : " + board);
+	}
+	*/
+	/*
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 SelectKey!");
+		board.setContent("새로 작성하는 내용 SelectKey!");
+		board.setWriter("새작성자2");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info("testInsertSelectKey : " + board);
+	}
+	*/
+	/*
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(10);		
+		log.info("testRead : " + board);
+	}
+	*/
+	/*
+	@Test
+	public void testDelete() {
+		int result = mapper.delete(7);		
+		log.info("testDelete : " + result);
+	}
+	*/
+	/*
+	@Test
+	public void testUpdate() {
+		
+		BoardVO board = new BoardVO();
+		
+		board.setBno(6);
+		board.setTitle("제목 수정!");
+		board.setContent("내용 수정~~~~~~!!!");
+		board.setWriter("user02");
+		
+		int result = mapper.update(board);
+		log.info("testUpdate : " + result);
+	}
+	*/
 }
