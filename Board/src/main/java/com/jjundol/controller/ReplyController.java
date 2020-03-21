f72f44100c532061027e9dbb6073ceae2aad564b
namgo <namgo@JUN-0109>
2020-03-21 20:23:21 +0900

    2020.03.21 - 파일 삭제

--- Board/src/main/java/com/jjundol/controller/ReplyController.java
+++ Board/src/main/java/com/jjundol/controller/ReplyController.java
@@ -1,9 +1,5 @@
 package com.jjundol.controller;
 
-import java.util.List;
-
-import javax.print.attribute.standard.Media;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jjundol.domain.Criteria;
--- Board/src/main/webapp/WEB-INF/views/board/get.jsp
+++ Board/src/main/webapp/WEB-INF/views/board/get.jsp
@@ -62,11 +62,7 @@
 	</div>
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">댓글 등록</h4>
-                </div>
+            <div class="modal-content">            
                 <div class="modal-body">
                 	<div class="form-group">
                 		<label>댓글</label>
